c1786991e32caa80343fb483996487335bdb52c9
Sheridan <sheridanmaustad@gmail.com>
2021-08-30 07:51:32 -0600

    commit tree 3ae38bd730bfeb51e4b0832ee7b8cd4ebc68b010

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array = array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+    array [1], array [2] = array [2], array [1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |item|
+        item[2] = "$"
+end
+end
